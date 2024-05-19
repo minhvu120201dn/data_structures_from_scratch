ef57594d24d65e2c00c45957af2b3ccf0bfe30cc
Quan Nguyen <programmerquannguyen@gmail.com>
2024-05-19 13:42:25 +0700

    update ultility functions for AVL

--- avl.py
+++ avl.py
@@ -1,13 +1,32 @@
 from data_structure import DataStructure
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
+
 class AVLTree(DataStructure):
     def __init__(self):
-        #TODO
-        pass
+        self.root = None
 
     def __len__(self) -> int:
         #TODO
         pass
+
+    def getHeight(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def updateHeight(self, node):
+        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
+
+    def getBF(self, node):
+        if not node:
+            return 0
+        return self.get_height(node.left) - self.get_height(node.right)
 
     def add(self, value:int) -> None:
         #TODO
